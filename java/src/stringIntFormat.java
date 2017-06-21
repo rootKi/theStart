bb003706bd5a656cdaa16c6133d34245a1d656eb
rootKi <kanoasoul@gmail.com>
2017-06-20 23:14:23 -0500

    stringIntFormat
    used the magic of delimiters well only one delimiter

--- java/src/stringIntFormat.java
+++ java/src/stringIntFormat.java
@@ -5,19 +5,22 @@ public class stringIntFormat {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        sc.useDelimiter(" ");
 
         System.out.println("Enter stuff: ");
+        String input = sc.nextLine();
+
+        String[] stringArr = input.split("\\s+");
 
         System.out.println("================================");
-//        for(int i=1;i<3;i++){
-            String s1 = sc.next();
-            int x = sc.nextInt();
+        for(int i=0;i<2;i++){
+//            String s1 = sc.next();
+//            int x = sc.nextInt();
 
-            System.out.printf("%-15", s1, x);
+            System.out.printf("%-15s", stringArr[i]);
+//            System.out.println(stringArr[i]);
 
-//        }
-        System.out.println("================================");
+        }
+        System.out.println("\n================================");
 
     }
 }

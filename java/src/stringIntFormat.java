b8640889420a39405fc5dce05eaec6bc53cf6828
rootKi <kanoasoul@gmail.com>
2017-06-21 19:43:41 -0500

    stringIntFormat
    Not sure why I didn't think of this before
    Get it together Sully

--- java/src/stringIntFormat.java
+++ java/src/stringIntFormat.java
@@ -1,26 +1,22 @@
+import com.sun.xml.internal.ws.util.StringUtils;
+
 import java.util.Scanner;
 
 public class stringIntFormat {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-
-        System.out.println("Enter stuff: ");
-        String input = sc.nextLine();
-
-        String[] stringArr = input.split("\\s+");
-
+        
         System.out.println("================================");
-        for(int i=0;i<2;i++){
-//            String s1 = sc.next();
-//            int x = sc.nextInt();
+        for(int i=0; i < 3; i++){
+            String s1 = sc.next();
+            int x = sc.nextInt();
 
-            System.out.printf("%-15s", stringArr[i]);
-//            System.out.println(stringArr[i]);
+            System.out.printf("%-15s", s1);
+            System.out.println(String.format("%03d", x));
 
         }
-        System.out.println("\n================================");
+        System.out.println("================================");
 
     }
 }
